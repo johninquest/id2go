c0b772b9e47d4d817aa969fd782d456133e232e3
john@home <johninquest@gmail.com>
2024-03-03 23:40:25 +0100

    improved category list 2

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -56,7 +56,7 @@ class AuthPage extends StatelessWidget {
                 // child: const Icon(Icons.home),
                 child: const Text(
                   'START',
-                  style: TextStyle(fontSize: 21.0, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 21.0),
                 ),
               ),
             )
